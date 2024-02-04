cc4e0cc95dc438605113508da8c55c8eacc925c1
Yulia Lomenko <angelama2408@gmail.com>
2024-02-04 18:54:45 +0100

    tpf

---
+++ src/lessons_02/CamelCase.java
@@ -0,0 +1,30 @@
+package lessons_02;
+
+/*
+@date 17.01.2024
+@author Sergey Bugaienko
+*/
+
+public class CamelCase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Кириллица - вывод текста");
+        System.out.println("");
+
+        System.out.println();
+    }
+
+//    psvm | main -> Enter | Tab
+//    Ctrl + / - закомментировать / раскомментировать строку
+//    однострочный комментарий
+//    Ctrl + Y - удаляет строку, на которой находится курсор
+//    Ctrl + Alt + L
+//    sout + Tab | Enter -> System.out.println();
+
+    /*
+    многострочный комментарии
+     */
+
+
+}
